fc11994e73014ffc1183ccf8e37ad9b2698c267f
ahmetyi <yahmet069998@gmail.com>
2020-08-18 00:16:48 +0200

    ikinci satir eklendi

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -3,8 +3,8 @@ package git_project_ikinci;
 public class deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Ikinci git projem");
+		System.out.println("Ikinci satir kodum");
 	}
 
 }
